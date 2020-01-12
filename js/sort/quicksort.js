5ebb033bb58418fdefb68257956ce71438d7f5db
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-01-12 11:37:55 +0300

    add quicksort

---
+++ js/sort/quicksort.js
@@ -0,0 +1,31 @@
+function quicksort(array, left = 0, right = array.length - 1) {
+  if (right <= left) {
+    return;
+  }
+  console.log(array);
+  const pivot = array[left];
+  const index = partition(array, left, right, pivot);
+  quicksort(array, left, index - 1);
+  quicksort(array, index, right);
+}
+
+function partition(array, left, right, pivot) {
+  while (left <= right) {
+    while (array[left] < pivot) {
+      left++;
+    }
+    while (array[right] > pivot) {
+      right--;
+    }
+    if (left <= right) {
+      [array[left], array[right]] = [array[right], array[left]];
+      left++;
+      right--;
+    }
+  }
+  return left;
+}
+
+quicksort([5, 8, 1, 3, 7, 9, 2]);
+// quicksort([0]);
+// quicksort([0, 1, 2, 3, 4]);
